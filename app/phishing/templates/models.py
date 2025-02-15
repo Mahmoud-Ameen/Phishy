20917a2e6205c7f7c5c7a267fdded9f715328cce
Mahmoud Ashraf <mahmoud612.ashraf@gmail.com>
2025-02-15 04:38:08 +0200

    create db model and domain entity for phishing templates

---
+++ app/phishing/templates/models.py
@@ -0,0 +1,16 @@
+from ...extensions import db
+
+
+class TemplateModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    level = db.Column(db.Integer, nullable=False)
+    subject = db.Column(db.Text)
+    content = db.Column(db.Text)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'level': self.level,
+            'subject': self.subject,
+            'content': self.content
+        }
