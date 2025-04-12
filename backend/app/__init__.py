6179ca5a6db8ca20d8f44a74a164c559adef55b4
Mahmoud Ashraf <mahmoud612.ashraf@gmail.com>
2025-04-12 11:40:56 +0200

    fix: disable strict trailing slashes to avoid redirecting preflight requests

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -11,6 +11,8 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+    app.url_map.strict_slashes = False
+
     CORS(app)
 
     # Initialize extensions
