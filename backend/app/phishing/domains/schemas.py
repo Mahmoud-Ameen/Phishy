34fd2414e352b6e9ac921fdd8819f36e2fd5d96e
Mahmoud Ashraf <mahmoud612.ashraf@gmail.com>
2025-05-03 14:02:22 +0300

    fix: fix domain regex in create domain endpoint

--- backend/app/phishing/domains/schemas.py
+++ backend/app/phishing/domains/schemas.py
@@ -2,4 +2,4 @@ from marshmallow import Schema, fields, validate
 
 
 class CreateDomainSchema(Schema):
-    domain_name = fields.String(required=True, validate=validate.Regexp(r'^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9]\.[a-zA-Z]{2,}$'))
+    domain_name = fields.String(required=True, validate=validate.Regexp(r'^(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$'))
